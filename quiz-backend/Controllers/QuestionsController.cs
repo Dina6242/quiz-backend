99fcdbde751269237cfe86ef010f2ee2bb4a4937
dinasamer559@yahoo.com <dinasamer559@yahoo.com>
2020-07-26 14:56:48 +0200

    add new method Put method and connect with entity framework

--- quiz-backend/Controllers/QuestionsController.cs
+++ quiz-backend/Controllers/QuestionsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace quiz_backend.Controllers
 {
@@ -22,10 +23,23 @@ namespace quiz_backend.Controllers
             return context.Questions;
         }
         [HttpPost]
-        public void Post([FromBody] Models.Question question)
+        public async Task<IActionResult> Post([FromBody] Models.Question question)
         {
             context.Questions.Add(question);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
+            return Ok(question);
         }
+        [HttpPut("{id}")]
+        public async Task<IActionResult>  Put(int id, [FromBody] Models.Question question)
+        {
+            if (id != question.ID)
+                return BadRequest();
+            context.Entry(question).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return Ok(question);
+
+        }
+
     }
 }
