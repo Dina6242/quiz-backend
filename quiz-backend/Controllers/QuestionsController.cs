58737aa0f9ad0265a8163ca6a22eee6d3ab69d8f
dinasamer559@yahoo.com <dinasamer559@yahoo.com>
2020-08-02 02:07:32 +0200

    show only quiz owend question

--- quiz-backend/Controllers/QuestionsController.cs
+++ quiz-backend/Controllers/QuestionsController.cs
@@ -22,9 +22,17 @@ namespace quiz_backend.Controllers
         {
             return context.Questions;
         }
+        [HttpGet("{quizId}")]
+        public IEnumerable<Models.Question> Get([FromRoute] int quizId)
+        {
+            return context.Questions.Where(q => q.QuizId== quizId);
+        }
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Models.Question question)
         {
+            var quiz = context.quiz.SingleOrDefault(q => q.ID == question.QuizId);
+            if (quiz == null)
+                return NotFound();
             context.Questions.Add(question);
             await context.SaveChangesAsync();
             return Ok(question);
